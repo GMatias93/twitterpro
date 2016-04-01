b3010767d6876c852730dbdb8e7f8f0bd1c51896
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-04-01 10:15:38 -0700

    edit: fixed code indentation

--- public/tweetFactory.js
+++ public/tweetFactory.js
@@ -1,38 +1,40 @@
 angular.module('tweet.factory', [])
 
-.factory('tweetFactory', ['$http','$rootScope', function ($http, $rootScope) {
+.factory('tweetFactory', ['$http', '$rootScope', function($http, $rootScope) {
   var factory = {};
 
-  factory.getUserObj = function (handle, callback) {
-   console.log('getting info for handle: ', handle);
-   $http.post('//localhost:3000/userObj', {handle:handle}).then(function(user){
-    console.log('got back this user',user);
-    callback(user.data);
-  });
- };
-
- factory.getMyTweets = function() {
-  console.log('getting my tweets');
-  $http.get('//localhost:3000/myTweets')
-  .then(function(err, data) {
-    console.log('err', err, 'data', data);
-  });
-};
-
-
-var socket = io.connect('//localhost:3000');
-factory.stream = {
-  on: function (eventName, callback) {
-    socket.on(eventName, function () {
-      var args = arguments;
-      $rootScope.$apply(function () {
-        callback.apply(socket, args);
-      });
+  factory.getUserObj = function(handle, callback) {
+    console.log('getting info for handle: ', handle);
+    $http.post('//localhost:3000/userObj', {
+      handle: handle
+    }).then(function(user) {
+      console.log('got back this user', user);
+      callback(user.data);
     });
-  }
-};
-var counter = {};
-factory.handleTweet = function (tweet) {
+  };
+
+  factory.getMyTweets = function() {
+    console.log('getting my tweets');
+    $http.get('//localhost:3000/myTweets')
+      .then(function(err, data) {
+        console.log('err', err, 'data', data);
+      });
+  };
+
+
+  var socket = io.connect('//localhost:3000');
+  factory.stream = {
+    on: function(eventName, callback) {
+      socket.on(eventName, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          callback.apply(socket, args);
+        });
+      });
+    }
+  };
+  var counter = {};
+  factory.handleTweet = function(tweet) {
     var tweetArr = tweet.text.split(' ');
     // loop trough the words in the tweet
     for (var i = 0; i < tweetArr.length; i++) {
@@ -45,29 +47,30 @@ factory.handleTweet = function (tweet) {
         counter[tweetArr[i]]++;
         // reset the root scope arrays
         $rootScope.hashTagKeys = [];
-        $rootScope.hashTagValues = [[]];
+        $rootScope.hashTagValues = [
+          []
+        ];
         // loop through the counter
         for (var key in counter) {
           if (counter[key] > 3 && key.length < 17) {
             var phase = $rootScope.$$phase;
             if (phase == '$apply' || phase == '$digest') {
-                //push key
-                $rootScope.hashTagKeys.push(key);
-                // push value
-                $rootScope.hashTagValues[0].push(counter[key]);  
-              } else {
-                $rootScope.$apply(function(){
+              //push key
+              $rootScope.hashTagKeys.push(key);
+              // push value
+              $rootScope.hashTagValues[0].push(counter[key]);
+            } else {
+              $rootScope.$apply(function() {
                 //push key
                 $rootScope.hashTagKeys.push(key);
                 // push value
                 $rootScope.hashTagValues[0].push(counter[key]);
               });
-              }
-            } 
+            }
           }
         }
       }
-    };
-    return factory;
-  },
-  ]);
+    }
+  };
+  return factory;
+}, ]);

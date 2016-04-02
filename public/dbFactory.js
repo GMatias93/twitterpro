c754fba360936e2eb6b51ed1bcba875afe27f284
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-04-02 16:34:05 -0700

    fix: removed console logs from dbFactory

--- public/dbFactory.js
+++ public/dbFactory.js
@@ -7,37 +7,25 @@ angular.module('db.factory', [])
 
   // use '/all/true' to get all models
   factory.getModel = function(type, searchTerms, callback) {
-    console.log('getting models');
-    console.log('type: ', type);
     $http.get(Server.url + 'api/models/' + type + searchTerms).then(function(results) {
-      console.log('successs in getting models', results.data);
       callback(results.data);
     });
   };
 
   factory.deleteModel = function(type, searchTerms, callback) {
-    console.log('deleting models');
-    console.log('type: ', type);
     $http.delete(Server.url + 'api/models/' + type + searchTerms).then(function(results) {
-      console.log('success in deleting model');
       callback(results.data);
     });
   };
 
   factory.updateModel = function(type, payload, callback) {
-    console.log('updating model');
-    console.log('type: ', type);
     $http.put(Server.url + 'api/models/' + type, payload).then(function(results) {
-      console.log('success in updating model');
       callback(results.data);
     });
   };
 
   factory.createModel = function(type, payload, callback) {
-    console.log('creating model');
-    console.log('type', type);
     $http.post(Server.url + 'api/models/' + type, payload).then(function(results) {
-      console.log('success in creating model');
       callback(results.data);
     });
   };
